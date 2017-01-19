04ad5884ef0558bb1215ff59a955959d6e077886
AnyISalIn <anyisalin@gmail.com>
2017-01-19 11:15:26 +0800

    send html table

--- cabot_alert_email/models.py
+++ cabot_alert_email/models.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from os import environ as env
-
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives, EmailMessage
 from django.core.urlresolvers import reverse
 from django.template import Context, Template
+from django.template.loader import get_template
 from cabot.cabotapp.alert import AlertPlugin
+from cabot.cabotapp.models import StatusCheckResult
 
 import requests
 import logging
-
-email_template = """服务 {{ service.name }} {% if service.overall_status == service.PASSING_STATUS %} 恢复正常 {% else %} 问题很大 {% endif %}
-{% if service.overall_status != service.PASSING_STATUS %}
-{% for check in service.all_failing_checks %}
-{{ check.name }} - 类别: {{ check.check_category }} - 级别: {{ check.get_importance_display }}{% endfor %}
-{% endif %}
-"""
 
 
 class EmailAlert(AlertPlugin):
@@ -30,7 +24,8 @@ class EmailAlert(AlertPlugin):
         c = Context({
             'service': service,
             'host': settings.WWW_HTTP_HOST,
-            'scheme': settings.WWW_SCHEME
+            'scheme': settings.WWW_SCHEME,
+	    'StatusCheckResult': StatusCheckResult
         })
         if service.overall_status != service.PASSING_STATUS:
             if service.overall_status == service.CRITICAL_STATUS:
@@ -38,10 +33,9 @@ class EmailAlert(AlertPlugin):
             subject = '服务 {} - {}'.format(service.name, service.overall_status)
         else:
             subject = '服务 {} 恢复正常'.format(service.name,)
-        t = Template(email_template)
-        send_mail(
-            subject=subject,
-            message=t.render(c),
-            from_email='Cabot <%s>' % env.get('CABOT_FROM_EMAIL'),
-            recipient_list=emails,
-        )
+        html_content= get_template('./alert.html').render(c)
+	from_email='Cabot <%s>' % env.get('CABOT_FROM_EMAIL')
+	msg = EmailMessage(subject, html_content, from_email, emails)
+        msg.content_subtype = 'html'
+	msg.send()
+
